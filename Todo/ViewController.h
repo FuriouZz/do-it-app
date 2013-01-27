7a26daf0dcb51170a8b240b08133b80d20a92ab6
Christophe Massolin <Christophe@MacBook-Pro-de-Christophe.local>
2013-01-27 19:13:31 +0100

    Initial Commit

---
+++ Todo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Todo
+//
+//  Created by Christophe Massolin on 27/01/13.
+//  Copyright (c) 2013 FuriouZz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Todo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Todo
+//
+//  Created by Christophe Massolin on 27/01/13.
+//  Copyright (c) 2013 FuriouZz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
